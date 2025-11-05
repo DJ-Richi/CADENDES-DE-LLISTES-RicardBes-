83ee45e111454b269e138a2eb6fc73d3dd9b7f20
DJ-Richi <rbesguimera@gmail.com>
2025-11-05 13:55:19 +0100

    Add files via upload

---
+++ RA2_2.4_17_RicardBes.py
@@ -0,0 +1,17 @@
+# Administració de Sistemes Informàtics en Xarxa
+# 
+# Autor: Ricard Bes Guimerà
+# Data: 05/11/2024
+# Versió: 1
+#
+# Descripció: Demana una paraula i guarda cada lletra en una llista.
+# Especificacions Programa que et guardi cada lletra a una llista.
+
+# Demanar una paraula a l'usuari
+paraula = input("Introdueix una paraula: ")
+
+# Guardar cada lletra en una llista
+llista_lletres = list(paraula)
+
+# Mostrar la llista de lletres
+print("Lletres de la paraula:", llista_lletres)
