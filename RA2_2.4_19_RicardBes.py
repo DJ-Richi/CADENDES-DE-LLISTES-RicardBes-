83f64126e367b4bb8d96a387b95ed7be85c0c7f3
DJ-Richi <rbesguimera@gmail.com>
2025-11-05 16:37:35 +0100

    Add files via upload

---
+++ RA2_2.4_19_RicardBes.py
@@ -0,0 +1,19 @@
+# Administració de Sistemes Informàtics en Xarxa
+# 
+# Autor: Ricard Bes Guimerà
+# Data: 04/11/2024
+# Versió: 1
+#
+# Descripció: Fes una llista de números i multiplica tots els elements per 2.
+# Especificacions Programa que fagi una llista de numeros i es multiplica tots els elements per el doble.
+
+# Crear una llista de números
+llista = [1, 2, 3, 4, 5]
+
+# Multiplicar cada element per 2
+llista_doble = []
+for num in llista:
+    llista_doble.append(num * 2)
+
+# Mostrar el resultat
+print(llista_doble)
