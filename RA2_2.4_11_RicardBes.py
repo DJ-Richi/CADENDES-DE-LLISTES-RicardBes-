b808c12f28c2a6d9bb32849513e7a20613f4bc26
DJ-Richi <rbesguimera@gmail.com>
2025-11-05 09:32:34 +0100

    Add files via upload

---
+++ RA2_2.4_11_RicardBes.py
@@ -0,0 +1,17 @@
+# Administració de Sistemes Informàtics en Xarxa
+# 
+# Autor: Ricard Bes Guimerà
+# Data: 04/11/2024
+# Versió: 1
+#
+# Descripció: Crea una llista amb 5 nombres i imprimeix el major i el menor.
+# Especificacions Programa que et fagi una llista amb nombres.
+
+llista_nombres = [18, 43, 8, 79, 99]
+
+major = max(llista_nombres)
+menor = min(llista_nombres)
+
+print("Llista de nombres:", llista_nombres)
+print("El nombre més gran és:", major)
+print("El nombre més petit és:", menor)
