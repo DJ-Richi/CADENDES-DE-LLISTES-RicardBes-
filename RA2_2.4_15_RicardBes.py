ad8d66a42c6b32fdffd7e70e4149069b8661bc1a
DJ-Richi <rbesguimera@gmail.com>
2025-11-05 13:49:07 +0100

    Add files via upload

---
+++ RA2_2.4_15_RicardBes.py
@@ -0,0 +1,17 @@
+# Administració de Sistemes Informàtics en Xarxa
+# 
+# Autor: Ricard Bes Guimerà
+# Data: 05/11/2024
+# Versió: 1
+#
+# Descripció: Crea una llista amb noms i ordena'ls alfabèticament.
+# Especificacions Programa que et crea una llista amb noms que els ordena alfabèticament
+
+# Crear una llista amb noms
+noms = ["Pelacho", "Floreal", "Beleta", "Perri", "Pataco"]
+
+# Ordenar la llista alfabèticament
+noms_ordenats = sorted(noms)
+
+# Mostrar la llista ordenada
+print("Noms ordenats:", noms_ordenats)
